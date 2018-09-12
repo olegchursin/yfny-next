e22868341dc210b42db835acb91f1dd9a6a0f70f
olegchursin <olegchursin@gmail.com>
2018-09-12 04:51:39 -0400

    Initial out subtree commit

---
+++ _next/d3fde1dd-d389-46d0-bf79-5db460a3dbce/page/presentations.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/presentations",function(){return{page:webpackJsonp([5],{1037:function(e,t,n){e.exports=n(1038)},1038:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(0),r=n.n(a),u=n(63),o=n(48);t.default=function(){return r.a.createElement(o.a,null,r.a.createElement(u.a,{title:"Презентации"}))}}},[1037]).default}});
